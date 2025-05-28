f9f5bf4a6ed39c918c393a753b512c9f7530adc8
chalkclone <chalkclone@gmail.com>
2025-05-28 11:40:17 +0200

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const lcdUrl = "https://lcd-osmosis.blockapsis.com"; // проверенный рабочий сервер
+  const wallet = "osmo1psaaa8z5twqgs4ahgqdxwl86eydmlwhesmv4s9";
+
+  try {
+    const balanceResp = await fetch(`${lcdUrl}/cosmos/bank/v1beta1/balances/${wallet}`);
+    if (!balanceResp.ok) throw new Error("Ошибка при получении баланса");
+
+    const balances = await balanceResp.json();
+    res.status(200).json({ balances });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
