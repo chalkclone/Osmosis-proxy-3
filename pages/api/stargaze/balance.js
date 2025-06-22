15f2c9e8000aaf77cbc0be875019fc17ad5008df
chalkclone <chalkclone@gmail.com>
2025-06-22 13:12:46 +0200

    Update balance.js

--- pages/api/stargaze/balance.js
+++ pages/api/stargaze/balance.js
@@ -1,13 +1,12 @@
 export default async function handler(req, res) {
   const address = "stars1psaaa8z5twqgs4ahgqdxwl86eydmlwhevugcdx";
-  const lcdUrl = `https://rest.stargaze-apis.com/cosmos/bank/v1beta1/balances/${address}`;
+  const url = `https://rest.stargaze-apis.com/cosmos/bank/v1beta1/balances/${address}`;
 
   try {
-    const response = await fetch(lcdUrl);
+    const response = await fetch(url);
     if (!response.ok) throw new Error("Ошибка при получении баланса Stargaze");
 
     const data = await response.json();
-
     res.status(200).json({ balances: data.balances });
   } catch (error) {
     res.status(500).json({ error: error.message });
