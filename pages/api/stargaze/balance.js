b58129e33d1e7570a80c2bad492b2db5a24fefa1
chalkclone <chalkclone@gmail.com>
2025-06-22 13:04:53 +0200

    Update balance.js

--- pages/api/stargaze/balance.js
+++ pages/api/stargaze/balance.js
@@ -1,21 +1,15 @@
-import axios from 'axios';
-
-const STARGAZE_LCD = 'https://lcd.stargaze-apis.com';
-const address = 'stars1psaaa8z5twqgs4ahgqdxwl86eydmlwhevugcdx'; // Замени на свой адрес, если нужно
-
 export default async function handler(req, res) {
+  const address = "stars1psaaa8z5twqgs4ahgqdxwl86eydmlwhevugcdx";
+  const lcdUrl = `https://rest.stargaze-apis.com/cosmos/bank/v1beta1/balances/${address}`;
+
   try {
-    const balancesRes = await axios.get(`${STARGAZE_LCD}/cosmos/bank/v1beta1/balances/${address}`);
-    const balances = balancesRes.data.balances.filter(b => parseFloat(b.amount) > 0);
+    const response = await fetch(lcdUrl);
+    if (!response.ok) throw new Error("Ошибка при получении баланса Stargaze");
 
-    const prettyBalances = balances.map(b => ({
-      denom: b.denom,
-      amount: parseFloat(b.amount) / 1e6,
-    }));
+    const data = await response.json();
 
-    res.status(200).json({ balances: prettyBalances });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Ошибка при получении баланса Stargaze' });
+    res.status(200).json({ balances: data.balances });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 }
