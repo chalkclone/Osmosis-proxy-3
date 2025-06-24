951094273518aeef800ee88e5c776e6e54bdc617
chalkclone <chalkclone@gmail.com>
2025-06-24 23:13:30 +0200

    Update transactions.js

--- pages/api/osmosis/transactions.js
+++ pages/api/osmosis/transactions.js
@@ -1,17 +1,17 @@
 export default async function handler(req, res) {
   const address = "osmo1psaaa8z5twqgs4ahgqdxwl86eydmlwhesmv4s9";
-  const url = `https://osmosis-api.polkachu.com/cosmos/tx/v1beta1/txs?events=transfer.recipient='${address}'&order_by=ORDER_BY_DESC&limit=5`;
+  const url = `https://osmosis-api.polkachu.com/cosmos/tx/v1beta1/txs?events=transfer.recipient='${address}'&order_by=ORDER_BY_DESC&limit=50`;
 
   try {
     const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Ошибка при получении транзакций Osmosis");
-    }
     const data = await response.json();
-console.log("Osmosis transactions response:", JSON.stringify(data, null, 2));
+
+    if (!response.ok || !data.tx_responses) {
+      throw new Error("Invalid response from Osmosis API");
+    }
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: "Ошибка при получении транзакций Osmosis", details: error.message });
   }
 }
