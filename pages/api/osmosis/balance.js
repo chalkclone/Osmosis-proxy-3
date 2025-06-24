11d973eb7b8e0ae61d843864cfb64f5210e39a6e
chalkclone <chalkclone@gmail.com>
2025-06-24 21:14:00 +0200

    Update balance.js

--- pages/api/osmosis/balance.js
+++ pages/api/osmosis/balance.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
-const address = "osmo1psaaa8z5twqgs4ahgqdxwl86eydmlwhesmv4s9";
-const url = https://osmosis-api.polkachu.com/cosmos/bank/v1beta1/balances/${address};
+  const address = "osmo1psaaa8z5twqgs4ahgqdxwl86eydmlwhesmv4s9";
+  const url = `https://osmosis-api.polkachu.com/cosmos/bank/v1beta1/balances/${address}`;
 
-try {
-const response = await fetch(url);
-if (!response.ok) throw new Error("Ошибка при получении баланса Osmosis");
-const data = await response.json();
-res.status(200).json({ balances: data.balances });
-} catch (error) {
-res.status(500).json({ error: error.message });
-}
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("Ошибка при получении баланса Osmosis");
+    const data = await response.json();
+    res.status(200).json({ balances: data.balances });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
