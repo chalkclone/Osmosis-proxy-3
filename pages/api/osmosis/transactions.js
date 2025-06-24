42454ae50a5136e4ea909408f4660dade77b28e8
chalkclone <chalkclone@gmail.com>
2025-06-24 23:04:23 +0200

    Update transactions.js

--- pages/api/osmosis/transactions.js
+++ pages/api/osmosis/transactions.js
@@ -8,6 +8,8 @@ export default async function handler(req, res) {
       throw new Error("Ошибка при получении транзакций Osmosis");
     }
     const data = await response.json();
+console.log("Osmosis transactions response:", JSON.stringify(data, null, 2));
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
