30d3cb1627bce035a4283eced31fc28ea113aaf8
chalkclone <chalkclone@gmail.com>
2025-06-24 23:25:18 +0200

    Update transactions.js

--- pages/api/osmosis/transactions.js
+++ pages/api/osmosis/transactions.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
   const address = "osmo1psaaa8z5twqgs4ahgqdxwl86eydmlwhesmv4s9";
   const query = encodeURIComponent(`events=transfer.recipient='${address}'`);
-  const url = `https://osmosis-api.polkachu.com/cosmos/tx/v1beta1/txs?query=${query}&pagination.limit=50&pagination.reverse=true`;
-
+ const url = `https://osmosis-api.polkachu.com/cosmos/tx/v1beta1/txs?events=transfer.recipient=${address}&order_by=ORDER_BY_DESC&limit=50`;
   try {
     const resp = await fetch(url);
     const data = await resp.json();
