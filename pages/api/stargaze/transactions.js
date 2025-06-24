b1a54f224a35f6d7f1eaf948b9f49609f8fe4b39
chalkclone <chalkclone@gmail.com>
2025-06-24 22:57:21 +0200

    Update transactions.js

--- pages/api/stargaze/transactions.js
+++ pages/api/stargaze/transactions.js
@@ -1,6 +1,8 @@
+// Файл: pages/api/stargaze/transactions.js
+
 export default async function handler(req, res) {
   const address = "stars1psaaa8z5twqgs4ahgqdxwl86eydmlwhevugcdx";
-  const url = `https://rest.stargaze-apis.com/cosmos/tx/v1beta1/txs?events=transfer.recipient=${address}&order_by=ORDER_BY_DESC&limit=5`;
+  const url = `https://rest.stargaze-apis.com/cosmos/tx/v1beta1/txs?events=transfer.recipient='${address}'&order_by=ORDER_BY_DESC&limit=5`;
 
   try {
     const response = await fetch(url);
