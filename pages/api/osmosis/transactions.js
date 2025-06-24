6f66acba4676483f84f5c79417dce049bf793dcc
chalkclone <chalkclone@gmail.com>
2025-06-24 21:15:14 +0200

    Update transactions.js

--- pages/api/osmosis/transactions.js
+++ pages/api/osmosis/transactions.js
@@ -1,13 +1,15 @@
 export default async function handler(req, res) {
-const address = "osmo1psaaa8z5twqgs4ahgqdxwl86eydmlwhesmv4s9";
-const url = https://osmosis-api.polkachu.com/cosmos/tx/v1beta1/txs?events=transfer.recipient='${address}'&order_by=ORDER_BY_DESC&limit=5;
+  const address = "osmo1psaaa8z5twqgs4ahgqdxwl86eydmlwhesmv4s9";
+  const url = `https://osmosis-api.polkachu.com/cosmos/tx/v1beta1/txs?events=transfer.recipient='${address}'&order_by=ORDER_BY_DESC&limit=5`;
 
-try {
-const response = await fetch(url);
-if (!response.ok) throw new Error("Ошибка при получении транзакций Osmosis");
-const data = await response.json();
-res.status(200).json(data);
-} catch (error) {
-res.status(500).json({ error: error.message });
-}
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Ошибка при получении транзакций Osmosis");
+    }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
