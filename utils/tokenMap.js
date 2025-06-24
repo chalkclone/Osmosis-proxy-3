9120273aef14fa5f55468e0ddc73e0687860e0ab
chalkclone <chalkclone@gmail.com>
2025-06-24 20:23:06 +0200

    Create tokenMap.js

---
+++ utils/tokenMap.js
@@ -0,0 +1,13 @@
+const TOKEN_MAP = {
+  uosmo: "OSMO",
+  ustars: "STARS",
+  "ibc/498A07...": "USDC", // замените на реальные IBC-адреса ваших токенов
+  "ibc/84CF82...": "USDC",
+  "ibc/9DF365...": "USDC",
+  "ibc/ED07A3...": "USDC",
+  uatom: "ATOM",
+  utia: "TIA",
+  uusdc: "USDC",
+};
+
+export default TOKEN_MAP;
