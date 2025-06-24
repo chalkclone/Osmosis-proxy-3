cdf2c13f8493df82158b18f545be93b192825a42
chalkclone <chalkclone@gmail.com>
2025-06-24 23:17:11 +0200

    Update transactions.js

--- pages/api/stargaze/transactions.js
+++ pages/api/stargaze/transactions.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const address = "stars1psaaa8z5twqgs4ahgqdxwl86eydmlwhevugcdx";
-  const url = `https://rest.stargaze-apis.com/cosmos/tx/v1beta1/txs?events=transfer.recipient='${address}'&order_by=ORDER_BY_DESC&limit=50`;
+  const url = `https://rest.stargaze-apis.com/cosmos/tx/v1beta1/txs?events=transfer.recipient=${address}&order_by=ORDER_BY_DESC&limit=50`;
 
   try {
     const response = await fetch(url);
@@ -12,6 +12,9 @@ export default async function handler(req, res) {
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Ошибка при получении транзакций Stargaze", details: error.message });
+    res.status(500).json({
+      error: "Ошибка при получении транзакций Stargaze",
+      details: error.message,
+    });
   }
 }
