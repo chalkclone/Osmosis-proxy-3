f192edd4faae0c7c24ed7b80e323c71ed6b9c4f0
chalkclone <chalkclone@gmail.com>
2025-06-24 21:28:08 +0200

    Update transactions.js

--- pages/api/stargaze/transactions.js
+++ pages/api/stargaze/transactions.js
@@ -1,13 +1,16 @@
 export default async function handler(req, res) {
-const address = "stars1psaaa8z5twqgs4ahgqdxwl86eydmlwhevugcdx";
-const url = https://rest.stargaze-apis.com/cosmos/tx/v1beta1/txs?events=transfer.recipient='${address}'&order_by=ORDER_BY_DESC&limit=5;
+  const address = "stars1psaaa8z5twqgs4ahgqdxwl86eydmlwhevugcdx";
+  const url = `https://rest.stargaze-apis.com/cosmos/tx/v1beta1/txs?events=transfer.recipient='${address}'&order_by=ORDER_BY_DESC&limit=5`;
 
-try {
-const response = await fetch(url);
-if (!response.ok) throw new Error("Ошибка при получении транзакций Stargaze");
-const data = await response.json();
-res.status(200).json(data);
-} catch (error) {
-res.status(500).json({ error: error.message });
-}
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Ошибка при получении транзакций Stargaze");
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
